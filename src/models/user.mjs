b92f6b7817f9b5ba40ce3c5b6f519200b1b47ed4
Andrii <andre40000k@gmail.com>
2025-03-24 12:17:46 +0200

    fixed model of user

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -1,9 +1,12 @@
+import { string } from "joi";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
   email: { type: String, unique: true },
-  password: String,
-  googleId: String, 
+  age: Number,
+  password: String
 });
 
 export const User = mongoose.model("User", userSchema);
--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -2,20 +2,15 @@ import express from "express";
 import {loginHandler} from "../auth/loginHandler.mjs";
 import {registerHandler} from "../auth/registerHandler.mjs";
 import {logoutHandler} from "../auth/logoutHandler.mjs"
-<<<<<<< HEAD
 import { initGoogleAuth, googleAuthCallback } from "../auth/googleAuthHandler.mjs";
-=======
->>>>>>> 04f05a8d3463610105fab753d9daa5c5b3676928
-
 const router = express.Router();
 
 router.post("/register", registerHandler);
 router.post("/login", loginHandler);
-router.get("/logout", logoutHandler)
 
 router.get("/google", initGoogleAuth);
 router.get("/google/callback", googleAuthCallback);
 
-router.get("/logout", logoutHandler)
+router.get("/logout", logoutHandler);
 
 export default router;
